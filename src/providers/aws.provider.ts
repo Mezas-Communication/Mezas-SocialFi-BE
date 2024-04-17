884f28fa9f22b925eac3b18eb9d783471199c39a
HDuong1803 <dohaiduong1803@gmail.com>
2024-04-17 11:43:31 +0700

    feat: add ipfs service

--- src/providers/aws.provider.ts
+++ src/providers/aws.provider.ts
@@ -11,7 +11,7 @@ const s3 = new AWS.S3({
   }
 })
 
-const uploadFile = async (imageBuffer: Buffer): Promise<string> => {
+const uploadFileS3 = async (imageBuffer: Buffer): Promise<string> => {
   const fileName = keccak256(imageBuffer)
 
   const uploadParams = {
@@ -27,7 +27,7 @@ const uploadFile = async (imageBuffer: Buffer): Promise<string> => {
   return fileName
 }
 
-const readFile = async (key: string) => {
+const readFileS3 = async (key: string) => {
   const params = {
     Bucket: Constant.S3_BUCKET_NAME,
     Key: key
@@ -41,4 +41,4 @@ const readFile = async (key: string) => {
   }
 }
 
-export { uploadFile, readFile }
+export { uploadFileS3, readFileS3 }
